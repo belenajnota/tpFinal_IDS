fc85b250f5fb58aeadbb6999bae379ab58e78efe
Ian-Albornoz <ialbornoz@fi.uba.ar>
2025-07-06 15:40:34 -0300

    CRUD (GET, POST, DELETE, PUT) de photocards hecho

--- backend/src/api.js
+++ backend/src/api.js
@@ -3,15 +3,26 @@ const {
   getAllPhotocards,
   getPhotocard,
   createPhotocard,
+  deletePhotocard,
+  updatePhotocards,
 } = require("./photocard.js");
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+//Health route
+app.get("/Api/health", (req, res) => {
+  res.json({ status: "OK" });
+});
 
+// CRUD de photocards
 app.get("/api/photocards", async (req, res) => {
   const photocards = await getAllPhotocards();
+  //verificador de errores de servidor
+  if (photocards == false) {
+    res.status(500).send("An error occurred");
+  }
   res.json(photocards);
 });
 
@@ -20,6 +31,10 @@ app.get("/api/photocards/:num", async (req, res) => {
   if (photocard == undefined) {
     res.status(404).send("Photocard Not Found");
   }
+  if (photocard == false) {
+    res.status(500).send("An error occurred");
+  }
+
   res.json(photocard);
 });
 
@@ -51,13 +66,41 @@ app.post("/api/photocards", async (req, res) => {
     req.body.precio,
     req.body.precio_comprada
   );
-
+  if (photocard == false) {
+    res
+      .status(500)
+      .send("An error occurred while attempting to create the photocard.");
+  }
   res.status(201).send("Photocard created successfully");
 });
 
-//Health route
-app.get("/Api/health", (req, res) => {
-  res.json({ status: "OK" });
+app.delete("/api/photocards/:id", async (req, res) => {
+  const photocard = await getPhotocard(req.params.id);
+  if (photocard == undefined) {
+    res.status(404).send("Not Found: The requested resource was not found.");
+  }
+
+  const status = await deletePhotocard(req.params.id);
+  if (status == false) {
+    res.status(500).send("An error occurred ");
+  }
+  res.status(200).send("Item deleted successfully.");
+});
+
+app.put("/api/photocards/:id", async (req, res) => {
+  const photocard = await getPhotocard(req.params.id);
+  if (photocard == undefined) {
+    res.status(404).send("Not Found: The requested resource was not found.");
+  }
+
+  const columns = Object.keys(req.body); //obtengo las llaves del json
+  const values = Object.values(req.body); //obtengo los valores del json
+  const status = await updatePhotocards(req.params.id, columns, values);
+
+  if (status == false) {
+    res.status(500).send("An error occurred ");
+  }
+  res.status(200).send("Changes saved successfully.");
 });
 
 app.listen(PORT, () => {
--- backend/src/photocard.js
+++ backend/src/photocard.js
@@ -9,19 +9,26 @@ const dbClient = new Pool({
 });
 
 async function getAllPhotocards() {
-  const response = await dbClient.query("SELECT * FROM photocards");
-  return response.rows;
+  try {
+    const response = await dbClient.query("SELECT * FROM photocards");
+    return response.rows;
+  } catch (e) {
+    return false;
+  }
 }
 
 async function getPhotocard(id) {
-  const response = await dbClient.query(
-    "SELECT * FROM photocards WHERE id = $1",
-    [id]
-  );
-  if (response.rowCount === 0) {
-    return undefined;
-  } else {
+  try {
+    const response = await dbClient.query(
+      "SELECT * FROM photocards WHERE id = $1",
+      [id]
+    );
+    if (response.rowCount === 0) {
+      return undefined;
+    }
     return response.rows[0];
+  } catch (e) {
+    return false;
   }
 }
 
@@ -33,16 +40,51 @@ async function createPhotocard(
   precio,
   precio_comprada
 ) {
-  const response = await dbClient.query(
-    "INSERT INTO photocards (nombre_idol,nombre_grupo,nombre_album,imagen,precio,precio_comprada) VALUES ($1,$2,$3,$4,$5,$6)",
-    [nombre_idol, nombre_grupo, nombre_album, imagen, precio, precio_comprada]
-  );
+  try {
+    await dbClient.query(
+      "INSERT INTO photocards (nombre_idol,nombre_grupo,nombre_album,imagen,precio,precio_comprada) VALUES ($1,$2,$3,$4,$5,$6)",
+      [nombre_idol, nombre_grupo, nombre_album, imagen, precio, precio_comprada]
+    );
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
 
-  return response.rows[0];
+async function deletePhotocard(id) {
+  try {
+    await dbClient.query("DELETE FROM photocards WHERE id = $1", [id]);
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
+
+async function updatePhotocards(id, columns, newValues) {
+  let max = columns.length;
+
+  try {
+    for (let i = 0; i < max; i++) {
+      //recorre las columnas y valores para updatear los datos uno por uno
+      let column = columns[i];
+      let newValue = newValues[i];
+      await dbClient.query(
+        `UPDATE photocards SET "${column}" = $1 WHERE id = $2`,
+        [newValue, id]
+      );
+    }
+
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
 
 module.exports = {
   getAllPhotocards,
   getPhotocard,
   createPhotocard,
+  deletePhotocard,
+  updatePhotocards,
 };
