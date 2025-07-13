54b8794a0fc4839e1ae96e8e5853520ea85cd234
Ian-Albornoz <ialbornoz@fi.uba.ar>
2025-07-13 14:57:04 -0300

    agrego cors para poder ejecutar front y back en localhost

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 const {
   getPhotocards,
