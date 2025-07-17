470f9201d70d45655c6a6b7eb2c1ed079ff46fd7
Ian-Albornoz <ialbornoz@fi.uba.ar>
2025-07-16 21:31:52 -0300

    agrego el boton de filtro en albums

--- frontend/Pagina-de-Administrador/k-card-albums/code.js
+++ frontend/Pagina-de-Administrador/k-card-albums/code.js
@@ -7,7 +7,6 @@ async function getAlbums() {
     const albums = Object.values(data);
 
     albums.forEach((album) => {
-      console.log(album);
       // se crea la fila para toda la informacion
       const newContainer = document.createElement("div");
       newContainer.className = "card-album is-flex is-flex-direction-column";
@@ -23,9 +22,19 @@ async function getAlbums() {
       }
 
       const nameAlbum = document.createElement("p");
-      nameAlbum.className = "card-text";
+      nameAlbum.className = "card-text card-name";
       nameAlbum.innerHTML = album.nombre;
       newContainer.appendChild(nameAlbum);
+
+      const groupAlbum = document.createElement("p");
+      groupAlbum.className = "card-text card-group";
+      groupAlbum.innerHTML = album.grupo;
+      newContainer.appendChild(groupAlbum);
+
+      const versionAlbum = document.createElement("p");
+      versionAlbum.className = "card-text card-version";
+      versionAlbum.innerHTML = album.version_album;
+      newContainer.appendChild(versionAlbum);
 
       // se crean los botones para las acciones ver, borrar y modificar
       const newButtonVer = document.createElement("a");
@@ -85,3 +94,139 @@ async function getAlbums() {
 }
 
 getAlbums();
+
+//codigo para que funcione el boton filtro
+document.addEventListener("DOMContentLoaded", () => {
+  // Obtener el elemento del dropdown
+  const dropdown = document.querySelector(".dropdown");
+  // Obtener el disparador del dropdown (el botón)
+  const dropdownTrigger = dropdown.querySelector(".dropdown-trigger button");
+
+  // Función para alternar la clase 'is-active'
+  function toggleDropdown() {
+    dropdown.classList.toggle("is-active");
+  }
+
+  // Añadir evento de clic al disparador
+  dropdownTrigger.addEventListener("click", toggleDropdown);
+
+  // Cerrar el dropdown si se hace clic fuera de él
+  document.addEventListener("click", (event) => {
+    // Si el clic no fue dentro del dropdown ni en el disparador del dropdown
+    if (!dropdown.contains(event.target) && event.target !== dropdownTrigger) {
+      // Y si el dropdown está activo, entonces desactivarlo
+      if (dropdown.classList.contains("is-active")) {
+        dropdown.classList.remove("is-active");
+      }
+    }
+  });
+});
+
+async function completeSelects() {
+  // esto es para los select que filtran
+  try {
+    const albumBackendUrl = "http://localhost:3000/api/albums";
+    const responseAlbum = await fetch(albumBackendUrl);
+    const data = await responseAlbum.json();
+    const albums = Object.values(data);
+    const selectGroupAlbum = document.getElementById("filter-by-group");
+    const selectVersionAlbum = document.getElementById("filter-by-version");
+    //hago verificadores para hacer el select de los filtros
+    const groupAlbum = albums.map((album) => album.grupo);
+    const uniqueGroups = [...new Set(groupAlbum)];
+    const versionAlbum = albums.map((album) => album.version_album);
+    const uniqueVersions = [...new Set(versionAlbum)];
+    console.log(uniqueVersions);
+
+    for (let version of uniqueVersions) {
+      const newOptionVersionAlbum = document.createElement("option");
+      newOptionVersionAlbum.innerHTML = version;
+      newOptionVersionAlbum.value = version;
+      selectVersionAlbum.appendChild(newOptionVersionAlbum);
+    }
+    for (let group of uniqueGroups) {
+      const newOptionGroupAlbum = document.createElement("option");
+      newOptionGroupAlbum.innerHTML = group;
+      newOptionGroupAlbum.value = group;
+      selectGroupAlbum.appendChild(newOptionGroupAlbum);
+    }
+  } catch (e) {}
+}
+
+completeSelects();
+
+//codigo para que funcionen los filtros
+
+const selectOrderBy = document.getElementById("order-by");
+
+selectOrderBy.addEventListener("change", () => {
+  const containerCard = document.querySelector(".container-card");
+  const cards = Array.from(containerCard.querySelectorAll("div"));
+
+  if (selectOrderBy.value == "NameA-Z") {
+    const sortedCards = cards.sort((a, b) => {
+      return a
+        .querySelector(".card-name")
+        .textContent.localeCompare(b.querySelector(".card-name").textContent);
+    });
+
+    containerCard.innerHTML = "";
+
+    sortedCards.forEach((card) => {
+      containerCard.appendChild(card);
+    });
+  } else if (selectOrderBy.value == "NameZ-A") {
+    const sortedCards = cards.sort((a, b) => {
+      return b
+        .querySelector(".card-name")
+        .textContent.localeCompare(a.querySelector(".card-name").textContent);
+    });
+
+    containerCard.innerHTML = "";
+
+    sortedCards.forEach((card) => {
+      containerCard.appendChild(card);
+    });
+  }
+});
+
+const selectGroupFilter = document.getElementById("filter-by-group");
+const selectVersionFilter = document.getElementById("filter-by-version");
+let acc = 0;
+
+selectGroupFilter.addEventListener("change", () => {
+  const containerCard = document.querySelector(".container-card");
+  const cards = Array.from(containerCard.querySelectorAll("div"));
+
+  if (acc == 1) {
+    window.location.reload();
+  } else {
+    containerCard.innerHTML = "";
+    acc += 1;
+  }
+
+  cards.forEach((card) => {
+    const group = card.querySelector(".card-group").textContent;
+    if (group == selectGroupFilter.value) {
+      containerCard.appendChild(card);
+    }
+  });
+});
+selectVersionFilter.addEventListener("change", () => {
+  const containerCard = document.querySelector(".container-card");
+  const cards = Array.from(containerCard.querySelectorAll("div"));
+
+  if (acc == 1) {
+    window.location.reload();
+  } else {
+    containerCard.innerHTML = "";
+    acc += 1;
+  }
+
+  cards.forEach((card) => {
+    const version = card.querySelector(".card-version").textContent;
+    if (version == selectVersionFilter.value) {
+      containerCard.appendChild(card);
+    }
+  });
+});
