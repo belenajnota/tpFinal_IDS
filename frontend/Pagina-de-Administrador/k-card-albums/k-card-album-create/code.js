2fe8f37dc24924edfe46b80985dbe03f5e5bcedf
Ian-Albornoz <ialbornoz@fi.uba.ar>
2025-07-17 10:43:27 -0300

    rediseño de create album

--- frontend/Pagina-de-Administrador/k-card-albums/k-card-album-create/code.js
+++ frontend/Pagina-de-Administrador/k-card-albums/k-card-album-create/code.js
@@ -1,56 +1,36 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
-const ventaBackendUrl = "http://localhost:3000/api/ventas/" + id;
+const form = document.getElementById("formulario");
 
-const formulario = document.getElementById("formulario");
+const nameAlbum = document.getElementById("nameAlbum");
+const versionAlbum = document.getElementById("versionAlbum");
+const group = document.getElementById("group");
+const image = document.getElementById("image");
+const country = document.getElementById("country");
+const company = document.getElementById("company");
 
-async function getInfoForm() {
+nameAlbum.addEventListener("input", () => {
+  const preview = document.getElementById("cardName");
+  preview.innerHTML = nameAlbum.value || "Nombre del Álbum";
+});
+
+group.addEventListener("input", () => {
+  const preview = document.getElementById("cardGroup");
+  preview.innerHTML = group.value || "Grupo";
+});
+
+versionAlbum.addEventListener("input", () => {
+  const preview = document.getElementById("cardVersion");
+  preview.innerHTML = versionAlbum.value || "Version del Álbum";
+});
+
+const getInfo = () => {
   const requestJson = {};
-  const regexNumber = /^[0-9]+$/;
-  const inputNombreAlbum = document.getElementById("nombreAlbum");
-  const valueNombreAlbum = inputNombreAlbum.value;
-  if (valueNombreAlbum.length !== 0) {
-    requestJson.nombre = valueNombreAlbum;
-  }
-  const inputVersionAlbum = document.getElementById("versionAlbum");
-  const valueVersionAlbum = inputVersionAlbum.value;
+};
 
-  if (valueVersionAlbum.length !== 0) {
-    requestJson.version_album = valueVersionAlbum;
-  }
-
-  const inputGrupo = document.getElementById("Grupo");
-  const valueGrupo = inputGrupo.value;
-  if (valueGrupo.length !== 0) {
-    requestJson.grupo = valueGrupo;
-  }
-
-  const regexHttp = /^https?:\/\//;
-  const inputImagen = document.getElementById("Imagen");
-  const valueImagen = inputImagen.value;
-  if (valueImagen.length !== 0 && regexHttp.test(valueImagen)) {
-    requestJson.imagen = valueImagen;
-  }
-
-  const inputFechaLanzamiento = document.getElementById("fechaLanzamiento");
-  const valueFechaLanzamiento = inputFechaLanzamiento.value;
-  if (valueFechaLanzamiento.length !== 0) {
-    requestJson.fecha_lanzamiento = valueFechaLanzamiento;
-  }
-  const inputPrecioAlbum = document.getElementById("Precio");
-  const valuePrecioAlbum = inputPrecioAlbum.value;
-  if (valuePrecioAlbum.length !== 0 && regexNumber.test(valuePrecioAlbum)) {
-    const precioAlbumEntero = parseInt(valuePrecioAlbum);
-    requestJson.precio = precioAlbumEntero;
-  }
-  console.log("funcion ejecutada");
-  return requestJson;
-}
-
-async function createVenta() {
-  const requestJson = await getInfoForm();
-  console.log(requestJson);
+async function createAlbum() {
+  const requestJson = console.log(requestJson);
   const postBackendUrl = "http://localhost:3000/api/albums";
   await fetch(postBackendUrl, {
     method: "POST",
@@ -63,9 +43,9 @@ async function createVenta() {
   alert("se creo un album");
 }
 
-formulario.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  createVenta();
+  createAlbum();
   setTimeout(() => {
     window.location.href = "../index.html?nocache=" + new Date().getTime();
   }, 3000);
