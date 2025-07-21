d25c59c0b1d758f6419b65386111e3405ceaa6ee
Ian-Albornoz <ialbornoz@fi.uba.ar>
2025-07-21 20:52:10 -0300

    cambios en photocard

--- frontend/Pagina-de-Administrador/k-card-photocard/k-card-photocard/code.js
+++ frontend/Pagina-de-Administrador/k-card-photocard/k-card-photocard/code.js
@@ -4,37 +4,91 @@ const id = urlParams.get("id");
 const photocardBackendUrl = "http://localhost:3000/api/photocards/" + id;
 
 async function getPhotocard() {
-  try {
-    const responsePhotocard = await fetch(photocardBackendUrl);
-    const photocard = await responsePhotocard.json();
-    //Llamo a todas la filas de la columna
-    console.log(photocard);
-    const imgPhotocard = document.getElementById("imgPhotocard");
-    if (photocard.imagen == null) {
-      imgPhotocard.src = "../../imagenes/no-img.jpeg";
-    } else {
-      imgPhotocard.src = photocard.imagen;
-    }
+  const response = await fetch(photocardBackendUrl);
+  const photocard = await response.json();
 
-    const nombrePhotocard = document.getElementById("nombre");
-    const precioCompra = document.getElementById("precio-compra");
-    const estado = document.getElementById("estado");
-    const fechaCompra = document.getElementById("fecha-compra");
-    //Appendeo la informacion a la tabla
-    nombrePhotocard.innerHTML = photocard.nombre;
-    precioCompra.innerHTML = "$" + photocard.precio_comprada;
-    estado.innerHTML = photocard.estado;
-    if (photocard.fecha_comprada !== null) {
-      const fecha = photocard.fecha_comprada;
-      const fechaLimpia = fecha.split("T")[0];
-      fechaCompra.innerHTML = fechaLimpia;
-    }
-  } catch (e) {
-    console.log(e);
+  const image = document.getElementById("imgAlbum");
+  const namePhotocard = document.getElementById("name-photocard");
+  const group = document.getElementById("group");
+  const price = document.getElementById("price");
+  const availability = document.getElementById("availability");
+
+  image.src = photocard.imagen;
+  namePhotocard.innerHTML = photocard.nombre;
+  group.innerHTML = photocard.grupo;
+  price.innerHTML = photocard.precio_comprada;
+  if (photocard.disponible) {
+    availability.innerHTML = "Disponible";
+  } else {
+    availability.innerHTML = "Vendida";
   }
+
+  const associatedAlbum = photocard.album;
+  const cardImage = document.getElementById("cardImage");
+  const cardName = document.getElementById("cardName");
+  const cardGroup = document.getElementById("cardGroup");
+  const cardVersion = document.getElementById("cardVersion");
+  const cardButton = document.getElementById("cardButton");
+
+  cardImage.src = associatedAlbum.imagen;
+  cardName.innerHTML = associatedAlbum.nombre;
+  cardGroup.innerHTML = associatedAlbum.grupo;
+  cardVersion.innerHTML = associatedAlbum.version;
+  cardButton.href =
+    "/frontend/Pagina-de-Administrador/k-card-albums/k-card-album/index.html?id=" +
+    associatedAlbum.id;
 }
 
 getPhotocard();
+
+const form = document.getElementById("formulario");
+
+async function completeSelects() {
+  // esto es para los select que filtran
+  try {
+    const albumBackendUrl = "http://localhost:3000/api/albums";
+    const responseAlbum = await fetch(albumBackendUrl);
+    const data = await responseAlbum.json();
+    const albums = Object.values(data);
+    const albumPhotocard = document.getElementById("albumPhotocard");
+    const responsePhotocard = await fetch(photocardBackendUrl);
+    const photocard = await responsePhotocard.json();
+
+    albums.forEach((album) => {
+      const newOptionAlbum = document.createElement("option");
+      newOptionAlbum.innerHTML = album.nombre;
+      newOptionAlbum.value = album.id;
+      albumPhotocard.appendChild(newOptionAlbum);
+    });
+
+    const optionAlbum = Array.from(albumPhotocard.querySelectorAll("option"));
+    optionAlbum.forEach((option) => {
+      if (option.value == photocard.album.id) {
+        option.selected = true;
+      }
+    });
+  } catch (e) {}
+}
+
+completeSelects();
+
+function isValidInput(input) {
+  // No permite repeticiones como "aaa"
+  const regexVariety = /^(?!.*(.)\1+).+$/;
+  // Solo letras, espacios, guiones, tildes, etc.
+  const regexLetters = /^[A-Za-zÁÉÍÓÚáéíóúÑñüÜ' -]+$/;
+
+  return regexVariety.test(input) && regexLetters.test(input);
+}
+
+function verifyImage(path) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+    img.src = path;
+  });
+}
 
 //codigo para que funcione el modal
 document.addEventListener("DOMContentLoaded", () => {
@@ -89,86 +143,82 @@ document.addEventListener("DOMContentLoaded", () => {
       event.preventDefault(); // Esto detiene el envío del formulario y la recarga de la página.
       async function getInfoForm() {
         const requestJson = {};
+
         const regexNumber = /^[0-9]+$/;
-        const inputNombrePhotocard = document.getElementById("nombrePhotocard");
-        const valueNombrePhotocard = inputNombrePhotocard.value;
-        if (valueNombrePhotocard.length !== 0) {
-          requestJson.nombre = valueNombrePhotocard;
-        }
 
-        const inputPrecioPhotocard = document.getElementById("precioCompra");
-        const valuePrecioPhotocard = inputPrecioPhotocard.value;
+        const inputNamePhotocard = document.getElementById("namePhotocard");
+        const valueNamePhotocard = inputNamePhotocard.value;
         if (
-          valuePrecioPhotocard.length !== 0 &&
-          regexNumber.test(valuePrecioPhotocard)
+          valueNamePhotocard.length !== 0 &&
+          isValidInput(valueNamePhotocard)
         ) {
-          const precioPhotocardEntero = parseInt(valuePrecioPhotocard);
-          requestJson.precio_comprada = precioPhotocardEntero;
+          requestJson.nombre = valueNamePhotocard;
         }
 
-        const regexHttp = /^https?:\/\//;
-        const inputImagen = document.getElementById("Imagen");
-        const valueImagen = inputImagen.value;
-        if (valueImagen.length !== 0 && regexHttp.test(valueImagen)) {
-          requestJson.imagen = valueImagen;
-        }
-        const inputFechaCompra = document.getElementById("fechaCompra");
-        const valueFechaCompra = inputFechaCompra.value;
-        if (valueFechaCompra.length !== 0) {
-          requestJson.fecha_comprada = valueFechaCompra;
-        }
-        const selectEstado = document.getElementById("Estado");
-        const valueEstado = selectEstado.value;
-        if (valueEstado !== "Selecciona un Estado") {
-          requestJson.estado = valueEstado;
+        const inputGroup = document.getElementById("Group");
+        const valueGroup = inputGroup.value;
+        if (valueGroup.length !== 0 && isValidInput(valueGroup)) {
+          requestJson.grupo = valueGroup;
         }
 
-        console.log("funcion ejecutada");
+        const inputPrice = document.getElementById("Price");
+        const valuePrice = inputPrice.value;
+        if (valuePrice.length !== 0 && regexNumber.test(valuePrice)) {
+          requestJson.empresa = valuePrice;
+        }
 
-        inputNombrePhotocard.value = "";
-        inputVersionphotocard.value = "";
-        inputGrupo.value = "";
-        inputImagen.value = "";
-        inputFechaLanzamiento.value = "";
-        inputPreciophotocard.value = "";
+        const inputImage = document.getElementById("Image");
+        const imagePath = "/images/photocards/" + inputImage.value;
+        if (await verifyImage(imagePath)) {
+          requestJson.imagen = imagePath;
+        } else if (inputImage.value == "sin-imagen") {
+          requestJson.imagen = "/images/resources/no-img.jpeg";
+        }
+        if (albumPhotocard.value !== "") {
+          requestJson.album_id = parseInt(albumPhotocard.value);
+        }
+
+        inputNamePhotocard.value = "";
+        inputPrice.value = "";
+        inputGroup.value = "";
+        inputImage.value = "";
+
         return requestJson;
       }
 
-      async function updatephotocard() {
-        const requestJson = await getInfoForm();
-        console.log(requestJson);
-        const patchBackendUrl = "http://localhost:3000/api/photocards/" + id;
-        await fetch(patchBackendUrl, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(requestJson),
-        });
-
-        console.log("Hubo cambios en el robos");
+      async function updatePhotocard() {
+        try {
+          const requestJson = await getInfoForm();
+          if (Object.keys(requestJson).length > 0) {
+            const patchBackendUrl = photocardBackendUrl;
+            await fetch(patchBackendUrl, {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(requestJson),
+            });
+            alert("se modifico la photocard");
+            setTimeout(() => {
+              window.location.reload();
+            }, 3000);
+          }
+        } catch (e) {
+          console.log(e);
+        }
       }
 
-      updatephotocard();
-      //aca hacer el patch
-
-      closeModal(formDentroModal.closest(".modal"));
-
-      alert("Formulario procesado y cambios guardados");
-      setTimeout(() => {
-        window.location.reload();
-      }, 3000);
+      updatePhotocard();
     });
   }
 });
+const deleteButtonAlbum = document.getElementById("deleteButton");
 
-const deleteButton = document.getElementById("deleteButton");
-
-deleteButton.addEventListener("click", () => {
-  const mensajeBorrar = prompt(
+deleteButtonAlbum.addEventListener("click", () => {
+  const deleteMessage = prompt(
     "¿Estas seguro de borrar esta photocard? Si desea eliminar escriba 'Eliminar-photocard'"
   );
-  async function deletephotocard() {
+  async function deleteAlbum() {
     try {
       await fetch(photocardBackendUrl, {
         method: "DELETE",
@@ -181,106 +231,10 @@ deleteButton.addEventListener("click", () => {
       alert("Ocurrio un error al intentar eliminar la photocard");
     }
   }
-  if (mensajeBorrar == "Eliminar-photocard") {
-    deletephotocard();
+  if (deleteMessage == "Eliminar-photocard") {
+    deleteAlbum();
     setTimeout(() => {
       window.location.href = "../index.html?nocache=" + new Date().getTime();
     }, 3000);
   }
 });
-
-//photocards asociadas
-const photocardsBackendUrl = "http://localhost:3000/api/photocards";
-
-const AlbumsBackendUrl = "http://localhost:3000/api/albums";
-
-async function getAlbums() {
-  try {
-    const response = await fetch(AlbumsBackendUrl);
-    const albums = await response.json();
-
-    const responsePhotocard = await fetch(photocardBackendUrl);
-    const photocard = await responsePhotocard.json();
-
-    albums.forEach((album) => {
-      if (photocard.id_album == album.id) {
-        // se crea la fila para toda la informacion
-        const newContainer = document.createElement("div");
-        newContainer.className =
-          "card-photocard is-flex is-flex-direction-column";
-        // se crean y se appendean los datos en la fila
-        const imgAlbum = document.createElement("img");
-        if (album.imagen == null) {
-          imgAlbum.src = "../imagenes/no-img.jpeg";
-          newContainer.appendChild(imgAlbum);
-        } else {
-          imgAlbum.src = album.imagen;
-          newContainer.appendChild(imgAlbum);
-        }
-
-        const nombreAlbum = document.createElement("h2");
-        nombreAlbum.className = "nombre-photocard";
-        nombreAlbum.innerHTML = album.nombre;
-        newContainer.appendChild(nombreAlbum);
-
-        // se crean los botones para las acciones ver, borrar y modificar
-        const newButtonVer = document.createElement("a");
-        newButtonVer.innerHTML = "Ver";
-        newButtonVer.className = "button";
-        newButtonVer.id = "button-card";
-        newButtonVer.href =
-          "../../k-card-albums/k-card-album/index.html?id=" + album.id;
-        newContainer.appendChild(newButtonVer);
-
-        const newButtonBorrar = document.createElement("a");
-        newButtonBorrar.innerHTML = "Borrar";
-        newButtonBorrar.className = "button ";
-        newButtonBorrar.id = "button-card";
-        newContainer.appendChild(newButtonBorrar);
-
-        newButtonBorrar.addEventListener("click", () => {
-          const mensajeBorrar = prompt(
-            "¿Estas seguro de borrar este album? Si desea eliminar escriba 'Eliminar-album'"
-          );
-          const albumBackendUrl =
-            "http://localhost:3000/api/albums/" + album.id;
-          async function deleteAlbum() {
-            try {
-              await fetch(albumBackendUrl, {
-                method: "DELETE",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              });
-              console.log("Se elimino correctamente");
-            } catch (e) {
-              alert("Ocurrio un error al intentar eliminar el album");
-            }
-          }
-          if (mensajeBorrar == "Eliminar-album") {
-            deleteAlbum();
-            setTimeout(() => {
-              window.location.reload();
-            }, 3000);
-          }
-        });
-
-        const newButtonModificar = document.createElement("a");
-        newButtonModificar.innerHTML = "Modificar";
-        newButtonModificar.className = "button";
-        newButtonModificar.id = "button-card";
-        newButtonModificar.href =
-          "../../k-card-albums/k-card-album-update/index.html?id=" + album.id;
-
-        newContainer.appendChild(newButtonModificar);
-
-        const containerCard = document.getElementById("container-card");
-        containerCard.appendChild(newContainer);
-      }
-    });
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-getAlbums();
